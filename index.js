5ab300eccaf9a230bc14022d68a259d1ee869875
Max <egorovteam2019@yandex.ru>
2021-10-26 21:32:04 +0300

    1-task

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// 6 kyu - Array.diff 
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+//Solution is:
+
+function arrayDiff(a, b) {
+    let result = [];
+ 
+     for (let n of a) {
+        if (!b.includes(n)) {
+          result.push(n);
+        }
+     }
+     return result
+ }
