8c171f3b5c8fd46e0a53f5d96186d35f110bb1de
MaxOF <egorovteam2019@yandex.ru>
2022-04-05 18:54:24 +0300

    arrays

--- index.js
+++ index.js
@@ -74,3 +74,35 @@ function even_or_odd(number) {
       return "Odd"
     }
 }
+
+//6 kyu
+// Arrays Similar
+
+//Write a function that determines whether the passed in arrays are similar. Similar means they contain the same elements, and the same number of occurrences of elements.
+//
+// const arr1 = [1, 2, 2, 3, 4],
+//       arr2 = [2, 1, 2, 4, 3],
+//       arr3 = [1, 2, 3, 4],
+//       arr4 = [1, 2, 3, "4"]
+//
+// arraysSimilar(arr1, arr2); // Should equal true
+// arraysSimilar(arr2, arr3); // Should equal false
+// arraysSimilar(arr3, arr4); // Should equal false
+
+//Solution is:
+
+
+function arraysSimilar(arr1, arr2) {
+
+    return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort())
+
+//   if (arr1.length !== arr2.length) return false
+//   arr1.sort()
+//   arr2.sort()
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) return false
+//   }
+//   return true
+}
+
