65e5687cf05852ad608026481c0fda462096047e
MaxOF <egorovteam2019@yandex.ru>
2022-04-06 12:55:57 +0300

    new tasks from codewars

--- index.js
+++ index.js
@@ -234,3 +234,24 @@ function invert(array) {
 
     return result;
 }
+
+// 8 kyu
+// Hello, Name or World!
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+//
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+//
+//     Examples:
+//
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+//     * With `name` not given
+// or `name` = ""        => return "Hello, World!"
+
+function hello(name = '') {
+
+    if(!name.trim()) return 'Hello, World!'
+
+    return 'Hello, ' + name[0].toUpperCase() + name.slice(1).toLowerCase() + '!'
+}
