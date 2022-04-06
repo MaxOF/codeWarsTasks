c67bbda925ef2cd31d4bfeea267d1494bf5614f5
MaxOF <egorovteam2019@yandex.ru>
2022-04-06 12:56:43 +0300

    new tasks from codewars

--- index.js
+++ index.js
@@ -255,3 +255,22 @@ function hello(name = '') {
 
     return 'Hello, ' + name[0].toUpperCase() + name.slice(1).toLowerCase() + '!'
 }
+
+// 8 kyu
+// Grasshopper - Personalized Message
+//
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+//
+//     Use conditionals to return the proper message:
+//
+//     case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+function greet (name, owner) {
+    if (name === owner) {
+        return 'Hello boss'
+    } else {
+        return 'Hello guest'
+    }
+}
