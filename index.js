2934f4d094f61f258fa5a45d8fbb97bc9ed5f357
MaxOF <egorovteam2019@yandex.ru>
2022-04-06 12:54:55 +0300

    new tasks from codewars

--- index.js
+++ index.js
@@ -211,3 +211,26 @@ function spongeMeme(sentence) {
     }
     return result
 }
+
+// 8 kyu
+// Invert values
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+//
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+
+
+    let result = []
+
+    for(let i = 0; i < array.length; i++){
+        array[i] = array[i] * (-1)
+        result = array
+    }
+
+    return result;
+}
